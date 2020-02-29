b5bfc4e706ae55ad02d45e01fef70ac9c8129998
Jamil <jamil.zaitouny@gmail.com>
2020-02-29 15:49:38 +0200

    Added Book Model

---
+++ src/main/java/Main/Model/Book.java
@@ -0,0 +1,37 @@
+package Main.Model;
+
+public class Book extends BaseEntity<String> {
+    private String title;
+    private String authorName;
+
+    public Book(String IBAN, String title, String authorName) {
+        super(IBAN);
+        this.title = title;
+        this.authorName = authorName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Book){
+            return ((Book) obj).getId().equals(this.getId());
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "IBAN: " + this.getId() + "\n"
+                +"Title: " + this.title + "\n"
+                +"Author: " + this.authorName + "\n";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
