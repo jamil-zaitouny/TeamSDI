37bccc04077472228c15b2085e16487767bb59f8
Jamil <jamil.zaitouny@gmail.com>
2020-02-29 15:50:29 +0200

    Added Client Model

---
+++ src/main/java/Main/Model/Client.java
@@ -0,0 +1,38 @@
+package Main.Model;
+
+public class Client extends BaseEntity<Integer>{
+    private String name;
+    public static String className = "Clients";
+
+    public Client(int id, String name){
+        super(id);
+        this.name = name;
+    }
+
+    public Client(Client copyClient){
+        super(copyClient.getId());
+        this.name = copyClient.getName();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Client){
+            return ((Client) obj).getId() == this.getId();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientID: " + this.getId() + "\n"
+                +"ClientName" + name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
