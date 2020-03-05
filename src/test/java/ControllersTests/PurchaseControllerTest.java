3e839cabb78de01f67ec816d1e61f0961139c833
Monica Vladarean <mvladarean@yahoo.com>
2020-03-05 22:18:54 +0200

    added Purchase tests

---
+++ src/test/java/ControllersTests/PurchaseControllerTest.java
@@ -0,0 +1,68 @@
+package ControllersTests;
+
+import Controller.BookController;
+import Controller.ClientController;
+import Controller.PurchaseController;
+import Model.Book;
+import Model.Client;
+import Model.Exceptions.ValidatorException;
+import Model.Purchase;
+import Model.Validators.BookValidator;
+import Model.Validators.ClientValidator;
+import Model.Validators.PurchaseValidator;
+import Repository.RepositoryInMemory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+public class PurchaseControllerTest
+{
+    private PurchaseController purchaseController;
+    private ClientController clientController;
+    private BookController bookController;
+
+    @Before
+    public void setUp()  {
+        ClientValidator clientValidator = new ClientValidator();
+        BookValidator bookValidator = new BookValidator();
+        PurchaseValidator purchaseValidator = new PurchaseValidator();
+
+        RepositoryInMemory<Integer, Client> clientRepo = new RepositoryInMemory<>(clientValidator);
+        RepositoryInMemory<String, Book> bookRepo = new RepositoryInMemory<>(bookValidator);
+        RepositoryInMemory<Integer, Purchase> purchaseRepo = new RepositoryInMemory<>(purchaseValidator);
+
+        clientController = new ClientController(clientRepo);
+        bookController = new BookController(bookRepo);
+        purchaseController=new PurchaseController(purchaseRepo,clientRepo,bookRepo);
+
+        clientController.addClient(new Client(1,"a"));
+        clientController.addClient(new Client(2,"a"));
+
+        bookController.addBook(new Book("9781234567897","a","a"));
+        bookController.addBook(new Book("2781234567897","b","b"));
+
+        purchaseController.addPurchase(new Purchase(1,"9781234567897",1));
+        purchaseController.addPurchase(new Purchase(2,"2781234567897",2));
+    }
+
+    @After
+    public void tearDown() {
+        purchaseController=null;
+        clientController=null;
+        bookController=null;
+    }
+
+    @Test
+    public void addPurchaseTest() {
+        this.purchaseController.addPurchase(new Purchase(3,"2781234567897",2));
+    }
+
+    @Test
+    public void getAllTest()   {
+        Set<Purchase> purchases= this.purchaseController.getAllPurchases();
+        Assert.assertEquals(2, purchases.size());
+    }
+}
---
+++ src/test/java/InMemoryRepositoryTests/PurchaseInMemoryRepositoryTests.java
@@ -0,0 +1,61 @@
+package InMemoryRepositoryTests;
+
+import Model.Book;
+import Model.Client;
+import Model.Purchase;
+import Model.Validators.BookValidator;
+import Model.Validators.ClientValidator;
+import Model.Validators.PurchaseValidator;
+import Repository.RepositoryInMemory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+public class PurchaseInMemoryRepositoryTests {
+    private RepositoryInMemory purchases;
+    private RepositoryInMemory clients;
+    private RepositoryInMemory books;
+
+    @Before
+    public void setUp() throws Exception {
+        clients=new RepositoryInMemory(new ClientValidator());
+        clients.add(new Client(1,"a"));
+        clients.add(new Client(2,"a"));
+
+        books=new RepositoryInMemory(new BookValidator());
+        books.add(new Book("1234567890098","a","a"));
+        books.add(new Book("1234567890078","b","b"));
+        books.add(new Book("1334567890078","c","c"));
+
+        purchases=new RepositoryInMemory(new PurchaseValidator());
+        purchases.add(new Purchase(1,"1234567890098",1));
+        purchases.add(new Purchase(2,"1234567890078",2));
+        purchases.add(new Purchase(3,"1334567890078",1));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        clients=null;
+        books=null;
+        purchases=null;
+    }
+
+    @Test
+    public void testFindOne() throws Exception {
+
+        assertEquals("Failed",new Purchase(1,"1234567890098",1),purchases.findOne(1).get());
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        assertNotNull(purchases.add(new Purchase(7,"1234567890798",2)));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        assertNotNull(purchases.delete(2));
+    }
+}
