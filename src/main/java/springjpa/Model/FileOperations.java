fdabb03300cee9958917ed13beb065ce1ed6b6df
Monica Vladarean <mvladarean@yahoo.com>
2020-04-26 20:56:58 +0300

    deleted useless classes

--- src/main/java/springjpa/Model/FileOperations.java
+++
@@ -1,9 +0,0 @@
-package springjpa.Model;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-
-public interface FileOperations {
-    public String[] toCSV();
-    public Node toXML(Document document);
-}
